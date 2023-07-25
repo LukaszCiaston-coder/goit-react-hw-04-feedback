1aa5c359ee0b18b781de79c653e7058a307a81b5
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-07-25 10:18:56 +0200

    useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
+import { useState } from 'react';
+import { Statistics } from './Statistics/Statistic';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import './App.css';
+
 export const App = () => {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const changeState = (feedbackType) => {
+    setState((prevState) => ({
+      ...prevState,
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = state;
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = state;
+    const total = countTotalFeedback();
+    return total ? Math.round((good / total) * 100) : 0;
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="appContainer">
+    <Section title={'Please leave feedback'}>
+      <FeedbackOptions
+        options={Object.keys(state)}
+        onLeaveFeedback={changeState}
+      />
+      {countTotalFeedback() ? (
+        <Statistics
+          good={state.good}
+          neutral={state.neutral}
+          bad={state.bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+      </Section>
     </div>
   );
 };
---
+++ src/components/Statistics/Statistic.jsx
@@ -0,0 +1,30 @@
+import css from '../Statistics/Statistic.module.css';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <div className={css.statistics}>
+      <h2>Statistics</h2>
+      <p className={css.good}>
+        good: <span className={css.number}> {good}</span>
+      </p>
+      <p className={css.neutral}>
+        neutral: <span className={css.number}> {neutral}</span>
+      </p>
+      <p className={css.bad}>
+        bad: <span className={css.number}> {bad}</span>
+      </p>
+      <p>
+        total: <span className={css.total}> {total}</span>
+      </p>
+      <p>
+        Good percentage: <span className={css.percentage}>{positivePercentage}%</span>
+      </p>
+    </div>
+  );
+};
